874def228ba926e065a1b6eb9480f8a3e188d3b2
David Gurney <gurneyds@yahoo.com>
2017-09-20 15:52:00 -0600

    Further refinements

--- js/componentE.js
+++ js/componentE.js
@@ -10,14 +10,21 @@
 		constructor() {
 			super();
 			console.log("ComponentE constuctor");
-
-			this.attachShadow({mode: 'open'}).innerHTML = template;
 		}
 
 		connectedCallback() {
 			console.log("ComponentE connectedCallback");
-			this.value = this.getAttribute('value') || 0;
+
+			// The connected callback will be called whenever the component is moved to another place on the page, so
+			// it's probably a good idea to protect the shadowRoot from multiple calls.
+			if(!this.shadowRoot) {
+				this.attachShadow({mode: 'open'}).innerHTML = template;
+			}
+
+			// If the private variable has been set, then use it. Otherwise look for the attribute, else 0
+			this.value = this._value || (this.getAttribute('value') || 0);
 		}
+
 		disconnectedCallback() {
 			console.log("ComponentE disconnectedCallback");
 		}
@@ -27,11 +34,17 @@
 		}
 		set value(value) {
 			this._value = value;
-			this.shadowRoot.querySelector('#value').textContent = value;
 
-			// Pass the information to the sub-component
-			var compF = this.shadowRoot.querySelector('comp-f');
-			compF.value = this.value;
+			// It is possible that this could be called before the connected callback sets up the sub-components, so
+			// again protect it by looking at the shadowRoot. If the shadowRoot is there, then we would expect the
+			// sub-component to also be there.
+			if(this.shadowRoot) {
+				this.shadowRoot.querySelector('#value').textContent = value;
+
+				// Pass the information to the sub-component
+				var compF = this.shadowRoot.querySelector('comp-f');
+				compF.value = this.value + '-child';
+			}
 		}
 	}
 
