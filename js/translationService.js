398e775d5d94aaa176876b35f055850efe33fae5
David Gurney <gurneyds@yahoo.com>
2017-10-09 13:53:15 -0600

    Started looking at some translation approaches

---
+++ js/translationService.js
@@ -0,0 +1,9 @@
+(function() {
+	console.log("Translation service starting");
+	// Get the translation from the FS global variable on the page
+
+	// Walk the DOM looking for elements that have the "translations" property and pass in
+
+
+	console.log("Translation service finished");
+})();
